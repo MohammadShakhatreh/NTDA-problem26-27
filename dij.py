8ad42b786666f91de7f4567f851c82554f1bb239
Mohammad Shakhatreh <homdaa.sha@hotmail.com>
2019-04-12 22:51:50 +0300

    initial commit

---
+++ dij.py
@@ -0,0 +1,74 @@
+from minheap import MinHeap
+
+inputs = open('input.txt')
+s = inputs.readline().strip('\n').split(' ')
+
+N = int(s[0]) # number of nodes
+E = int(s[1]) # number of edges
+src = s[2]    # source node
+
+inputs = inputs.readlines()
+inputs = [ x.strip('\n').split(' ') for x in inputs ]
+
+graph = [
+    [] for _ in range(N)
+]
+
+# to convert letters to numbers
+index = 0
+mapping = {}
+
+for u, v, w in inputs:
+    # give u and v a number
+    if u not in mapping:
+        mapping[u] = index
+        index += 1
+
+    if v not in mapping:
+        mapping[v] = index
+        index += 1
+
+    # add the edge to the graph
+    graph[mapping[u]].append((mapping[v], int(w)))
+    graph[mapping[v]].append((mapping[u], int(w)))
+
+# reverse the mapping
+rmapping = { val : key for key, val in mapping.items() }
+
+src = mapping[src]
+
+def print_so_far():
+    for i in range(N):
+        offset = 4 if costs[i] == 10000 else costs[i]//10+2
+        print('{0:<{1}}'.format(rmapping[i], offset), end='')
+    print()
+
+    for cost in costs:
+        if cost == 10000:
+            print('inf', end=' ')
+        else:
+            print(cost, end = ' ')
+    print()
+
+# dijkstra part
+visited = [ False for _ in range(N) ]
+costs   = [ 10000 for _ in range(N) ]
+heap    = MinHeap() # for maintaining min cost element
+
+#initalization
+costs[src] = 0
+heap.insert((0, src))
+
+NN = ''
+while not heap.empty():
+    cw, cur = heap.poll()
+    visited[cur] = True
+
+    for v, w in graph[cur]:
+        if not visited[v] and costs[v] > costs[cur] + w:
+            costs[v] = costs[cur] + w
+            heap.insert((costs[v], v))
+
+    NN += rmapping[cur]
+    print(NN)
+    print_so_far()
---
+++ minheap.py
@@ -0,0 +1,86 @@
+from array import array
+
+class MinHeap:
+
+    def __init__(self):
+        self.size = 0
+        self.heap = []
+
+    def insert(self, x):
+        self.heap.append(x)
+        self.size += 1
+        self.up_heap()
+
+    def peek(self):
+        '''get max element'''
+        if self.size == 0:
+            raise Exception('Heap is empty!')
+        return self.heap[0]
+
+    def poll(self):
+        '''get max element and remove it'''
+        if self.size == 0:
+            raise Exception('Heap is empty!')
+
+        root = self.heap[0]
+        self.heap[0] = self.heap[self.size - 1]
+
+        self.heap.pop()
+        self.size -= 1
+        self.down_heap()
+        return root
+
+    def up_heap(self):
+        '''move last element up to it's normal pos'''
+        index = self.size - 1
+        while self.has_parent(index) and self.heap[index] < self.parent(index):
+            self.heap[index], self.heap[self.parent_index(index)] = \
+                self.heap[self.parent_index(index)], self.heap[index]
+            index = self.parent_index(index)
+
+    def down_heap(self):
+        '''move the root element down to it's normal pos'''
+        index = 0
+        while self.has_left(index):
+            min_index = self.left_index(index)
+            if self.has_right(index) and self.right(index) < self.left(index):
+                min_index = self.right_index(index)
+
+            if self.heap[index] < self.heap[min_index]:
+                break
+            else:
+                self.heap[index], self.heap[min_index] = \
+                    self.heap[min_index], self.heap[index]
+
+            index = min_index
+
+    def empty(self):
+        return self.size == 0
+
+    #helper methods
+    def parent(self, index):
+        return self.heap[self.parent_index(index)]
+
+    def parent_index(self, index):
+        return (index - 1) >> 1
+
+    def has_parent(self, index):
+        return self.parent_index(index) >= 0
+
+    def left(self, index):
+        return self.heap[self.left_index(index)]
+
+    def right(self, index):
+        return self.heap[self.right_index(index)]
+
+    def has_left(self, index):
+        return self.left_index(index) < self.size
+
+    def has_right(self, index):
+        return self.right_index(index) < self.size
+
+    def left_index(self, index):
+        return (index << 1) + 1
+
+    def right_index(self, index):
+        return (index << 1) + 2
